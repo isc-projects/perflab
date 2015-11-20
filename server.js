afe938796ae0b26ccf25b4f581b0f6ec6cd13e98
Ray Bellis <ray@isc.org>
2015-11-20 16:18:37 +0000

    improved row handling

--- server.js
+++ server.js
@@ -9,15 +9,7 @@ var mongoUrl = 'mongodb://localhost/perflab';
 var repoUrl = 'ssh://repo.isc.org/proj/git/prod/bind9';
 var perfPath = '/home/ray/bind-perflab';
 
-function firstOrReject(a) {
-	return new Promise((resolve, reject) => {
-		if (Array.isArray(a) && a.length > 0) {
-			resolve(a[0]);
-		} else {
-			reject({stderr: 'no entry found'});
-		}
-	});
-}
+var notNull = (o) => new Promise((res, rej) => o === null ? rej('npe') : res(o));
 
 function install(config) {
 	var agent = new ServerAgent(perfPath, repoUrl, config);
@@ -28,14 +20,14 @@ function install(config) {
 
 function get_config(db, config_name) {
 	return db.collection('config')
-		.find({name: config_name}).next();
-		// .then(firstOrReject);
+		.findOne({name: config_name})
+		.then(notNull);
 }
 
 try {
 	MongoClient.connect(mongoUrl).then((db) => {
 		var close = () => db.close();
-		var res = get_config(db, "v9_9");
+		var res = get_config(db, "v9_10");
 		res.then(close, close);
 		return res.then(install);
 	}).catch(console.error);
