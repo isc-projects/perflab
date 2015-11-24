37359487b16f98d847fde82434dac394f39149c7
Ray Bellis <ray@isc.org>
2015-11-24 10:49:39 +0000

    tieing it together a bit

--- database.js
+++ database.js
@@ -11,15 +11,28 @@ class Database {
 		var db;
 		var connect = () => MongoClient.connect(url).then((_db) => db = _db);
 		var open = () => db ? Promise.resolve(db) : connect();
-		var close = () => db.close();
+		var close = () => {
+			db.close();
+			db = null;
+		}
 
 		var getConfig = (db, name) => 
 			db.collection('config')
 				.findOne({name})
 				.then(notNull);
 
+		var insertRun = (db, results) =>
+			db.collection('run')
+				.insert(results);
+
 		this.getConfig = (name) => {
 			var res = open().then((db) => getConfig(db, name));
+			res.then(close, close);
+			return res;
+		}
+
+		this.insertRun = (results) => {
+			var res = open().then((db) => insertRun(db, results));
 			res.then(close, close);
 			return res;
 		}
--- server.js
+++ server.js
@@ -9,16 +9,20 @@ var mongoUrl = 'mongodb://localhost/perflab';
 var perfPath = '/home/ray/bind-perflab';
 var repoUrl = 'ssh://repo.isc.org/proj/git/prod/bind9';
 
-function install(config) {
+function run(config) {
 	var agent = new BindAgent(config, perfPath, repoUrl);
-	agent.on('stdout', console.log);
-	agent.on('stderr', console.error);
-	return agent.run();
+	var stdout = '', stderr = '';
+	agent.on('stdout', (t) => stdout += t);
+	agent.on('stderr', (t) => stderr += t);
+	return agent.run().then((status) => {
+		return { config_id: config._id, status, stdout, stderr }
+	});
 }
 
 try {
 	var db = new Database(mongoUrl);
-	db.getConfig("v9_10").then(install).catch(console.error);
+
+	db.getConfig("v9_10").then(run).then(db.insertRun).catch(console.error);
 } catch (e) {
 	console.error('catch: ' + e);
 }
