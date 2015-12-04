174e27955b194e6f8649685ba6c269c7c9e4cc57
Ray Bellis <ray@isc.org>
2015-12-04 20:06:30 +0000

    count extractor

--- dnsperf-agent.js
+++ dnsperf-agent.js
@@ -12,10 +12,20 @@ class DNSPerfAgent extends Executor {
 		let args = config.args = config.args || {};
 		let queryset = config.queryset || 'default';
 
+		var getCount = (results) => {
+			if (results.status === 0 && results.stdout) {
+				var match = results.stdout.match(/Queries per second:\s+(.*)$/m);
+				if (match) {
+					results.count = +match[1];
+				}
+			}
+			return results;
+		}
+
 		this.run = () => {
 			let args = ['-p', 8053, '-l', 30, '-d', `${path}/queryset/${queryset}`];
 			args = args.concat(args.dnsperf || []);
-			return this._ssh('localhost', '/usr/bin/dnsperf', args);
+			return this._ssh('localhost', '/usr/bin/dnsperf', args).then(getCount);
 		}
 	}
 }
