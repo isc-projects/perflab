40667902dedd3a923bb6e6e4e777064f4ccb615c
Ray Bellis <ray@isc.org>
2015-12-21 11:31:33 +0000

    forgotten from previous commits

---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+	mongoUrl:	'mongodb://localhost/perflab',
+	oplogUrl:	'mongodb://localhost/local',
+	perfPath:	'/home/ray/bind-perflab',
+	// repoUrl:	'ssh://repo.isc.org/proj/git/prod/bind9'
+	repoUrl:	'https://source.isc.org/git/bind9.git'
+};
