742f72e4289af6cb85519df3c0b1dfde0119e02d
Ray Bellis <ray@isc.org>
2015-12-23 15:32:01 +0000

    slight cleanup of graph code

--- www/js/ctrl-run-graph.js
+++ www/js/ctrl-run-graph.js
@@ -4,8 +4,57 @@
 
 var app = angular.module('perflabApp');
 
+app.controller('runGraphController',
+	['$scope', '$route', '$routeParams', '$location',
+	 'ConfigResource', 'RunResource',
+	function ($scope, $route, $routeParams, $location, ConfigResource, RunResource) {
+		var id = $routeParams.config_id;
+		$scope.graph = {
+			data: [],
+			options: {
+				errorBars: true, sigma: 1, showRangeSelector: false,
+				labels: ['x', 'Average', 'Range'],
+				series: {
+					Average: { label: 'Average' },
+					Range: { label: 'Range', highlightCircleSize: 0 }
+				},
+				xlabel: 'Date / Time',
+				ylabel: 'Queries per second',
+				axes: { y: { axisLabelWidth: 70 } },
+				height: 500,
+				legend: 'follow',
+				labelsSeparateLines: true,
+				dateWindow: [Date.now() - 2 * 86400000, Date.now()],
+				pointClickCallback: function(e, point) {
+					var id = $scope.graph.data[point.idx].id;
+					$location.path('/run/test/' + id + '/');
+					$route.reload();
+				},
+				plotter: plotter,
+				valueFormatter: formatter
+			}
+		};
+
+		$scope.config = ConfigResource.get({id: id});
+
+		RunResource.query({config_id: id}).$promise.then(function(data) {
+			$scope.graph.data = data.filter(function(run) {
+				return run.stats !== undefined && run.created !== undefined;
+			}).map(function(run) {
+				var s = run.stats;
+				var r = [
+					new Date(run.created),
+					[s.average, s.stddev],
+					[(s.min + s.max) / 2, (s.max - s.min) / 2],
+				];
+				r.id = run._id;		// slight hack - store ID as an array property
+				return r;
+			}).sort(function(a, b) { return a[0] - b[0] });
+		});
+	}
+]);
+
 function plotter(e) {
-	// This is the officially endorsed way to plot all the series at once.
 	if (e.seriesIndex !== 0) return;
 
 	var BAR_WIDTH = 4;
@@ -37,68 +86,20 @@ function plotter(e) {
 	}
 }
 
-app.controller('runGraphController',
-	['$scope', '$route', '$routeParams', '$location',
-	 'ConfigResource', 'RunResource',
-	function ($scope, $route, $routeParams, $location, ConfigResource, RunResource) {
-		var id = $routeParams.config_id;
-		$scope.graph = {
-			data: [],
-			options: {
-				errorBars: true, sigma: 1, showRangeSelector: false,
-				labels: ['x', 'Average', 'Range'],
-				legend: 'follow',
-				xlabel: 'Date / Time',
-				ylabel: 'Queries per second',
-				height: 500,
-				axes: { y: { axisLabelWidth: 70 } },
-				labelsSeparateLines: true,
-				dateWindow: [Date.now() - 2 * 86400000, Date.now()],
-				plotter: plotter,
-				series: {
-					Average: { label: 'Average' },
-					Range: { label: 'Range', highlightCircleSize: 0 }
-				},
-				valueFormatter: function(v, o, s, d, r, c) {
-					if (s === 'x') {
-						return  Dygraph.dateValueFormatter.apply(this, arguments);
-					} else if (s === 'Average') {
-						var range = d.getValue(r, c);
-						return Dygraph.numberValueFormatter.call(this, range[0], o, s, d, r, c)
-							   + '&nbsp;±&nbsp;' +
-							   Dygraph.numberValueFormatter.call(this, range[1], o, s, d, r, c);
-					} else {
-						var range = d.getValue(r, c);
-						return Dygraph.numberValueFormatter.call(this, range[0] - range[1], o, s, d, r, c)
-							   + '&nbsp;&dash;&nbsp;' +
-							   Dygraph.numberValueFormatter.call(this, range[0] + range[1], o, s, d, r, c);
-					}
-				},
-				pointClickCallback: function(e, point) {
-					var id = $scope.graph.data[point.idx].id;
-					$location.path('/run/test/' + id + '/');
-					$route.reload();
-				}
-			}
-		};
-
-		$scope.config = ConfigResource.get({id: id});
-
-		RunResource.query({config_id: id}).$promise.then(function(data) {
-			$scope.graph.data = data.filter(function(run) {
-				return run.stats !== undefined && run.created !== undefined;
-			}).map(function(run) {
-				var s = run.stats;
-				var r = [
-					new Date(run.created),
-					[s.average, s.stddev],
-					[(s.min + s.max) / 2, (s.max - s.min) / 2],
-				];
-				r.id = run._id;		// slight hack - store ID as an array property
-				return r;
-			}).sort(function(a, b) { return a[0] - b[0] });
-		});
+function formatter(v, o, s, d, r, c) {
+	if (s === 'x') {
+		return Dygraph.dateValueFormatter.apply(this, arguments);
+	} else if (s === 'Average') {
+		var range = d.getValue(r, c);
+		return Dygraph.numberValueFormatter.call(this, range[0], o, s, d, r, c)
+			   + '&nbsp;±&nbsp;' +
+			   Dygraph.numberValueFormatter.call(this, range[1], o, s, d, r, c);
+	} else {
+		var range = d.getValue(r, c);
+		return Dygraph.numberValueFormatter.call(this, range[0] - range[1], o, s, d, r, c)
+			   + '&nbsp;&dash;&nbsp;' +
+			   Dygraph.numberValueFormatter.call(this, range[0] + range[1], o, s, d, r, c);
 	}
-]);
+}
 
 })();
