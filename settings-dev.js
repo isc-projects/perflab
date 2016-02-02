f363ef0063d62c60ef66d35895bf4c38e0908410
Ray Bellis <ray@isc.org>
2016-02-02 16:08:56 +0000

    separated dev/live settings files

---
+++ settings-dev.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let schema = 'perflab';
+
+module.exports = {
+	path:		'/home/ray/bind-perflab',
+	mongo: {
+		schema,
+		url:	`mongodb://localhost/${schema}`,
+		oplog:	'mongodb://localhost/local'
+	},
+	repo: {
+		bind9: 	'https://source.isc.org/git/bind9.git'
+	},
+	hosts: {
+		dns: {
+			server: 'localhost',
+			tester:	'localhost'
+		}
+	}
+};
