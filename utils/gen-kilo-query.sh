461436f9148b69582022795b15089ffdc9d0db32
Ray Bellis <ray@isc.org>
2016-02-05 02:52:56 -0800

    fixed kilo script

--- utils/gen-kilo-query.sh
+++ utils/gen-kilo-query.sh
@@ -7,8 +7,8 @@ count=1000000
 
 for c in `seq 0 1000`; do
   for i in `seq -w 0 $max`; do
-    echo dom000$i.example A
-    echo dom000$i.example AAAA
-    echo dom000$i.example MX
+    echo dom00$i.example A
+    echo dom00$i.example AAAA
+    echo dom00$i.example MX
   done
 done | shuf -n $count
