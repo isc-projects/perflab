3847f147bf38382d30699078c4ce47fc8f6172a0
Ray Bellis <ray@isc.org>
2016-02-08 11:21:05 -0500

    large zone fixes

--- scripts/gen-mega-zone.sh
+++ scripts/gen-mega-zone.sh
@@ -4,8 +4,10 @@ cat <<-__EOT__;
 \$TTL 3600
 @	IN SOA ns1.example. dns.example. 2016010101 86400 3600 86400 86400
 	IN NS ns1.example.
+ns1	IN A 127.0.0.1
+	IN AAAA ::
 __EOT__
 
 for i in `seq -w 0 999999`; do
-  echo -e "$i	IN NS ns1.dom.example."
+  echo -e "dom$i	IN NS ns1.example."
 done
