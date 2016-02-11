fd8a8dc5759e9963bc98ed69b17b0214d50f1314
Ray Bellis <ray@isc.org>
2016-02-11 04:56:55 -0500

    more zone configs

---
+++ scripts/gen-mega-records.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cat <<-__EOT__;
+\$TTL 3600
+@	IN SOA ns1.example. dns.example. 2016010101 86400 3600 86400 86400
+	IN NS ns1.example.
+ns1	IN A 127.0.0.1
+	IN AAAA ::
+__EOT__
+
+for i in `seq -w 0 999999`; do
+  echo -e "dom$i	IN A 127.0.0.1"
+  echo -e "dom$i	IN AAAA ::1"
+done
