0ce394c565124e66ff268703e1279167e32c6ddd
Ray Bellis <ray@isc.org>
2016-02-12 10:11:36 -0500

    updated zone/config gen scripts

--- scripts/gen-mega-delegation.sh
+++ scripts/gen-mega-delegation.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cat <<-__EOT__;
+cat <<__EOT__;
 \$TTL 3600
 @	IN SOA ns1.example. dns.example. 2016010101 86400 3600 86400 86400
-	IN NS ns1.example.
+	IN NS ns1
 ns1	IN A 127.0.0.1
-	IN AAAA ::
+	IN AAAA ::1
 __EOT__
 
 for i in `seq -w 0 999999`; do
