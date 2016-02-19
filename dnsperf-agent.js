5d5674b8dc66e70107eed335933f51ae3191a4ea
Ray Bellis <ray@isc.org>
2016-02-19 13:46:29 +0000

    collect intermediate dnsperf stats

--- dnsperf-agent.js
+++ dnsperf-agent.js
@@ -32,7 +32,10 @@ class DNSPerfAgent extends Executor {
 		// start 'dnsperf' passing it the given query set and additional args
 		this.run = () => {
 			let args = [].concat(settings.args.dnsperf || []);
-			args = args.concat(['-s', server, '-p', 8053, '-l', 30, '-d', `${path}/queryset/${queryset}`]);
+			args = args.concat([
+				'-s', server, '-p', 8053, '-S', 1,
+				'-l', 30, '-d', `${path}/queryset/${queryset}`
+			]);
 			args = args.concat(config.args.dnsperf || []);
 			return this._ssh(tester, cmd, args).then(getCount);
 		}
