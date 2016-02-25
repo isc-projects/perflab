7f0c5c51d97ce02cb8f4db0c69cb1f061c9f3cce
Ray Bellis <ray@isc.org>
2016-02-25 17:42:57 +0000

    major refactoring of build agents

--- scripts/test-server.js
+++ scripts/test-server.js
@@ -3,11 +3,22 @@
 'use strict';
 
 let	Database = require('../lib/database'),
-	Agents = require('../lib/agents');
+	Agents = require('../lib/agents'),
+	Promise = require('bluebird'),
+	settings = require('../settings');
 
-let db = new Database();
-db.getConfigById('56caec922ac471e80b900245').then((config) => {
-	let type = config.type || 'bind9';
-	let agent = new Agents[type].server(config);
-	return agent.run(config);
-}).then(console.log).catch(console.error);
+Promise.longStackTraces();
+
+if (process.argv.length < 3) {
+	console.error("please supply a config id");
+	process.exit();
+}
+
+let id = process.argv[2];
+let db = new Database(settings);
+
+db.getConfigById(id).then((config) => {
+	let type = config.type || 'bind';
+	let agent = new Agents[type].server(settings, config);
+	return agent.run(config).then(agent.stop);
+}).catch(console.trace);
