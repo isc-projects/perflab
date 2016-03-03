4efaf843e194cbc5cb16cd7a4bf2e8047512995d
Ray Bellis <ray@isc.org>
2016-03-03 13:48:15 +0000

    script to regen stats

---
+++ scripts/regen-all-stats.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+'use strict';
+
+let	Database = require('../lib/database'),
+	Agents = require('../lib/agents'),
+	Promise = require('bluebird'),
+	settings = require('../settings');
+
+Promise.longStackTraces();
+
+let dbo = new Database(settings);
+
+dbo.query((db) => {
+	db.collection('run').find({},{_id:1}).toArray().then((runs) => {
+		return Promise.each(runs, (run) => {
+			console.log(run._id);
+			return dbo.updateStatsByRunId(run._id);
+		});
+	});
+});
