202d0afcdda55af6476685d4567e81df64ef8814
Ray Bellis <ray@isc.org>
2017-04-07 12:23:21 +0100

    rearrange settings structures on per-agent basis

--- settings-dev-dhcp.js
+++ settings-dev-dhcp.js
@@ -9,20 +9,24 @@ module.exports = {
 		url:	`mongodb://localhost/${schema}`,
 		oplog:	'mongodb://localhost/local'
 	},
-	repo: {
-		bind:	'ssh://repo.isc.org/proj/git/prod/bind9.git',
-		dhcpd:	'ssh://repo.isc.org/proj/git/prod/dhcp.git',
-		kea4: 	'https://github.com/isc-projects/kea.git'
-	},
 	hosts: {
 		dhcp: {
 			server: '127.0.0.1',
 			tester:	'localhost'
 		}
 	},
-	command: {},
-	wrapper: {},
-	args: {},
 	testsPerRun: 10,
-	queueFilter: {type: {$in: ['kea4', 'kea6', 'dhcpd']}}
+	queueFilter: {type: {$in: ['kea4', 'kea6', 'dhcpd']}},
+
+	agents: {
+		bind: {
+			repo: { git: 'ssh://repo.isc.org/proj/git/prod/bind9.git' }
+		},
+		dhcpd: {
+			repo: { git: 'ssh://repo.isc.org/proj/git/prod/dhcp.git' }
+		},
+		kea4: {
+			repo: { git: 'https://github.com/isc-projects/kea.git' }
+		},
+	}
 };
