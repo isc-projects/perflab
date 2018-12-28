7edcc23216be15c9c2c866ba4867ebb92b6be89c
Ray Bellis <ray@isc.org>
2018-12-28 21:58:00 +0000

    whitespace

--- www/js/components.js
+++ www/js/components.js
@@ -5,38 +5,38 @@
 var app = angular.module('perflabApp');
 
 app.directive('cfgButtonQueue', function() {
-    return {
-        templateUrl: 'partials/config-queue.html'
+	return {
+		templateUrl: 'partials/config-queue.html'
 	}
 });
 
 app.directive('cfgButtonGraph', function() {
-    return {
-        template: '<a href="#/config/run/{{config._id}}/" class="icon-button btn btn-sm btn-primary"><span class="fas fa-chart-bar" title="Graph"></span></a>',
+	return {
+		template: '<a href="#/config/run/{{config._id}}/" class="icon-button btn btn-sm btn-primary"><span class="fas fa-chart-bar" title="Graph"></span></a>',
 	}
 });
 
 app.directive('cfgButtonList', function() {
-    return {
-        template: '<a href="#/config/run/{{config._id}}/list/" class="icon-button btn btn-sm btn-primary"><span class="fas fa-bars" title="List"></span></a>',
+	return {
+		template: '<a href="#/config/run/{{config._id}}/list/" class="icon-button btn btn-sm btn-primary"><span class="fas fa-bars" title="List"></span></a>',
 	}
 });
 
 app.directive('cfgButtonExport', function() {
-    return {
-        template: '<a href="/api/config/run/{{config._id}}/stats" class="icon-button btn btn-sm btn-primary"><span class="fas fa-download" title="Export CSV"></span></a>',
+	return {
+		template: '<a href="/api/config/run/{{config._id}}/stats" class="icon-button btn btn-sm btn-primary"><span class="fas fa-download" title="Export CSV"></span></a>',
 	}
 });
 
 app.directive('cfgButtonEdit', function() {
-    return {
-        template: '<a href="#/config/edit/{{config.type}}/{{config._id}}" class="icon-button btn btn-sm btn-primary"><span class="fas fa-cog" title="Edit"></span></a>',
+	return {
+		template: '<a href="#/config/edit/{{config.type}}/{{config._id}}" class="icon-button btn btn-sm btn-primary"><span class="fas fa-cog" title="Edit"></span></a>',
 	}
 });
 
 app.directive('runButtonMemory', function() {
-    return {
-        template: '<a href="#/run/memory/{{run._id}}/" class="fixed-button btn btn-sm btn-primary"><span class="fas fa-chart-line"></span>Memory</a>'
+	return {
+		template: '<a href="#/run/memory/{{run._id}}/" class="fixed-button btn btn-sm btn-primary"><span class="fas fa-chart-line"></span>Memory</a>'
 	}
 });
 
