df36925dbe6d11cb71406977c025fc067ca00f70
Ray Bellis <ray@isc.org>
2018-12-28 15:46:39 +0000

    use Angular components for common buttons

---
+++ www/js/components.js
@@ -0,0 +1,37 @@
+(function() {
+
+"use strict";
+
+var app = angular.module('perflabApp');
+
+app.directive('cfgButtonGraph', function() {
+    return {
+        template: '<a href="#/config/run/{{config._id}}/" class="icon-button btn btn-sm btn-primary"><span class="fas fa-chart-bar" title="Graph"></span></a>',
+	}
+});
+
+app.directive('cfgButtonList', function() {
+    return {
+        template: '<a href="#/config/run/{{config._id}}/list/" class="icon-button btn btn-sm btn-primary"><span class="fas fa-bars" title="List"></span></a>',
+	}
+});
+
+app.directive('cfgButtonExport', function() {
+    return {
+        template: '<a href="/api/config/run/{{config._id}}/stats" class="icon-button btn btn-sm btn-primary"><span class="fas fa-download" title="Export CSV"></span></a>',
+	}
+});
+
+app.directive('cfgButtonEdit', function() {
+    return {
+        template: '<a href="#/config/edit/{{config.type}}/{{config._id}}" class="icon-button btn btn-sm btn-primary"><span class="fas fa-cog" title="Edit"></span></a>',
+	}
+});
+
+app.directive('runButtonMemory', function() {
+    return {
+        template: '<a href="#/run/memory/{{run._id}}/" class="fixed-button btn btn-sm btn-primary"><span class="fas fa-chart-line"></span>Memory</a>'
+	}
+});
+
+})();
