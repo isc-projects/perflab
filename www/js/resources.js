a25508c517f476fb517376de4175e6268733ac93
Ray Bellis <ray@isc.org>
2019-03-11 09:40:38 +0000

    convert resources.js to ES6 syntax

--- www/js/resources.js
+++ www/js/resources.js
@@ -2,73 +2,57 @@
 
 'use strict';
 
-var module = angular.module('isc.resources', ['ngResource']);
+let module = angular.module('isc.resources', ['ngResource']);
 
 module.factory('TestResource', ['$resource',
-	function($resource) {
-		return $resource('/api/test/:id', { id: '@_id' }, {
+	($resource) => $resource('/api/test/:id', { id: '@_id' }, {
 			query: {
 				url: '/api/run/test/:run_id/',
 				isArray: true
 			}
-		});
-	}
+		})
 ]);
 
 module.factory('MemoryResource', ['$resource',
-	function($resource) {
-		return $resource('/api/run/memory/:run_id/');
-	}
+	($resource) => $resource('/api/run/memory/:run_id/')
 ]);
 
 module.factory('RunResource', ['$resource',
-	function($resource) {
-		return $resource('/api/run/:id', { id: '@_id' }, {
+	$resource => $resource('/api/run/:id', { id: '@_id' }, {
 			query: {
 				url: '/api/config/run/:config_id/',
 				isArray: true
 			}
-		});
-	}
+	})
 ]);
 
 module.factory('ConfigListResource', ['$resource',
-	function($resource) {
-		return $resource('/api/config_list/:id', { id: '@_id' });
-	}
+	($resource) => $resource('/api/config_list/:id', { id: '@_id' })
 ]);
 
 module.factory('ConfigResource', ['$resource',
-	function($resource) {
-		return $resource('/api/config/:id', { id: '@_id' }, {
+	($resource) => $resource('/api/config/:id', { id: '@_id' }, {
 			update: {
 				method: 'PUT',
 				transformResponse: (data) => angular.fromJson(data).value
 			}
-		});
-	}
+	})
 ]);
 
 module.factory('ServerAgentResource', ['$resource',
-	function($resource) {
-		return $resource('/api/agent/server/:agent', {}, {
+	($resource) => $resource('/api/agent/server/:agent', {}, {
 			query: { isArray: false }
-		});
-	}
+	})
 ]);
 
 module.factory('ClientAgentResource', ['$resource',
-	function($resource) {
-		return $resource('/api/agent/client/:agent', {}, {
+	($resource) => $resource('/api/agent/client/:agent', {}, {
 			query: { isArray: false },
-		});
-	}
+	})
 ]);
 
 module.factory('SettingsResource', ['$resource',
-	function($resource) {
-		return $resource('/api/settings/');
-	}
+	($resource) => $resource('/api/settings/')
 ]);
 
 })();
