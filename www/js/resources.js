b60c261fa1bbc1f22b1835b08829334d1e9ced4d
Ray Bellis <ray@isc.org>
2019-03-19 14:51:27 +0000

    formatting

--- www/js/resources.js
+++ www/js/resources.js
@@ -3,11 +3,11 @@ const module = angular.module('isc.resources', ['ngResource']);
 module.factory('TestResource',
 	['$resource',
 	($resource) => $resource('/api/test/:id', { id: '@_id' }, {
-			query: {
-				url: '/api/run/test/:run_id/',
-				isArray: true
-			}
-		})
+		query: {
+			url: '/api/run/test/:run_id/',
+			isArray: true
+		}
+	})
 ]);
 
 module.factory('MemoryResource',
@@ -17,11 +17,11 @@ module.factory('MemoryResource',
 
 module.factory('RunResource',
 	['$resource',
-	$resource => $resource('/api/run/:id', { id: '@_id' }, {
-			query: {
-				url: '/api/config/run/:config_id/',
-				isArray: true
-			}
+	($resource) => $resource('/api/run/:id', { id: '@_id' }, {
+		query: {
+			url: '/api/config/run/:config_id/',
+			isArray: true
+		}
 	})
 ]);
 
@@ -33,24 +33,24 @@ module.factory('ConfigListResource',
 module.factory('ConfigResource',
 	['$resource',
 	($resource) => $resource('/api/config/:id', { id: '@_id' }, {
-			update: {
-				method: 'PUT',
-				transformResponse: (data) => angular.fromJson(data).value
-			}
+		update: {
+			method: 'PUT',
+			transformResponse: (data) => angular.fromJson(data).value
+		}
 	})
 ]);
 
 module.factory('ServerAgentResource',
 	['$resource',
 	($resource) => $resource('/api/agent/server/:agent', {}, {
-			query: { isArray: false }
+		query: { isArray: false }
 	})
 ]);
 
 module.factory('ClientAgentResource',
 	['$resource',
 	($resource) => $resource('/api/agent/client/:agent', {}, {
-			query: { isArray: false },
+		query: { isArray: false },
 	})
 ]);
 
